2a8a11a495fbff4e772e2aea4b416db8c4c381c3
sachin gurav <guravsachin354@gmail.com>
2025-04-20 11:44:24 +0530

    code

---
+++ Java Practice/src/com/program/ReverseNumber.java
@@ -0,0 +1,13 @@
+package com.program;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		String s = "sachin";
+		System.out.println(s);
+		// TODO Auto-generated method stub
+
+	}
+
+}
